1db8d229a64518e4e9478459392d42efa070d79a
TingSyuanWang <vincent@fishboneapps.com>
2023-10-30 16:22:57 -0400

    feat: Redirecting new records to the edit page
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type {LinksFunction} from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form, Link,
   Links,
@@ -21,7 +21,7 @@ export const loader = async () => {
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export const links: LinksFunction = () => [
